dbf5e245bea0fd63f54ed237a9e907888081af88
gamemaster123356 <74640951+gamemaster123356@users.noreply.github.com>
2021-08-08 14:16:33 +0530

    Add files via upload

--- Programs/fileK.bat
+++ Programs/fileK.bat
@@ -4,28 +4,29 @@ cd Drivers
 call VGA.bat
 cd..
 cd Disk
-title Ker_OS -FileK
+title View OS -FileK
 set foldername=0
 set filename=0
 set file=0
 set delfile=0
 cls
 
-echo ========================================================================================================================
+:filek
+echo ════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
 echo.
-echo                       ##########    ##    ##            ##########   ##   ##
-echo                      ##            ##    ##            ##           ##  ##
-echo                     ##                  ##            ##           ## ##
-echo                   ##########    ##    ##            ##########    ####       Type help for a list of commands!
-echo                  ##            ##    ##            ##            ####
-echo                ##            ##    ##            ##            ##   ##
-echo              ##            ##    ##            ##             ##     ##
-echo             ##            ##    ##########    ##########     ##       ##
-echo                                                                             BETA 1.0
-echo ========================================================================================================================
+echo             ██████████    ██    ██            ██████████   ██   ██
+echo             ██                  ██            ██           ██  ██
+echo             ██                  ██            ██           ██ ██
+echo             ██████████    ██    ██            ██████████   ████            Type help for a list of commands!
+echo             ██            ██    ██            ██           ████
+echo             ██            ██    ██            ██           ██ ██
+echo             ██            ██    ██            ██           ██  ██
+echo             ██            ██    ██████████    ██████████   ██   ██
+echo                                                                       BETA 1.0
+echo ════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
 
 :commands
-set /p command=FileK: 
+set /p command=View OS FileK: 
 if "%command%"== "mkdir" goto :mkdir1
 if "%command%"== "mkfile" goto :mkfile
 if "%command%"== "paste" goto :paste
@@ -34,26 +35,30 @@ if "%command%"== "delete" goto :rmfile1
 if "%command%"== "list" goto :list
 if "%command%"== "opentxt" goto :opentxt1
 if "%command%"== "help" goto :help
+if "%command%"== "clear" goto :clear
+if "%command%"== "exit" goto :exit
 
 :unknowncmd
 echo Unkown Command! Type help
 goto commands
 
 :help
-echo FileK: =================================Help==================================
-echo FileK: 
-echo FileK:  help
-echo FileK:  mkdir
-echo FileK:  rmdir
-echo FileK:  copy
-echo FileK:  paste
-echo FileK:  rmfile
-echo FileK:  opentxt
-echo FileK:  list
-echo FileK:  
-echo FileK: =======================================================================
-echo FileK:                For full guide Check Out the GitHub!
-echo FileK: =======================================================================
+echo View OS FileK: ═════════════════════════════════Help══════════════════════════════════
+echo View OS FileK: 
+echo View OS FileK:  help        shows this command
+echo View OS FileK:  mkdir       make a folder
+echo View OS FileK:  rmdir       remove a folder
+echo View OS FileK:  copy        copy a file
+echo View OS FileK:  paste       paste a file from copy command
+echo View OS FileK:  rmfile      remove a file
+echo View OS FileK:  opentxt     open a text file
+echo View OS FileK:  list        List Files in disk
+echo View OS FileK:  exit        Exit FileK
+echo View OS FileK:  clear       Clear command line
+echo View OS FileK:  
+echo View OS FileK: ═══════════════════════════════════════════════════════════════════════
+echo View OS FileK:                  For full guide Check Out the GitHub!
+echo View OS FileK: ═══════════════════════════════════════════════════════════════════════
 goto commands
 
 :mkdir1
@@ -62,13 +67,6 @@ goto mkdir
 
 :mkdir
 mkdir %foldername%
-cd..
-cd Programs
-cd Filek_LOGS
-echo [%time%]_User_Made_Folder:%foldername%>> %date%.log
-cd..
-cd..
-cd Disk
 goto commands
 
 :mkfile
@@ -88,24 +86,15 @@ goto rmfile
 del"%delfile%"
 goto commands
 
+:clear
+cls
+goto filek
+
 :opentxt1
-cd..
-cd Programs
-cd Filek_LOGS
 set /p file=name-of-text-file: 
-cd..
-cd..
-cd Disk
 goto opentxt
 
 :opentxt
-cd..
-cd Programs
-cd Filek_LOGS
-echo [%time%]_User_Opened_File:%file%>> %date%.log
-cd..
-cd..
-cd Disk
 echo ╔═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
 echo ║ Text File %file%
 echo ╠═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
@@ -116,12 +105,11 @@ echo ╚════════════════════════
 goto commands
 
 :list
-cd..
-cd Programs
-cd Filek_LOGS
-echo [%time%]_User_Listed>> %date%.log
-cd..
-cd..
-cd Disk
 dir
 goto commands
+
+:exit
+cd..
+cd System
+set viewosopend=1
+call System.bat
